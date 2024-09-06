8ee0024863992f80c524d3cb1762472c84966216
alexanderek <Alexander.ek@live.se>
2024-09-06 11:47:45 +0200

    Initial commit

---
+++ NumbersGame/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace NumbersGame;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Random random =  new Random();
+        int secretNumber = random.Next(1, 20);
+        int attempts = 0;
+        bool isRunning = true;
+        
+        Console.WriteLine("Välkommen jag tänker på ett nummer kan du gissa vilket? \nDu får fem försök:");
+        
+        while (attempts < 5)
+        {
+            int guess = int.Parse(Console.ReadLine());
+            attempts++;
+                if (guessCheck(guess, secretNumber))
+                {
+                    Console.WriteLine($"Grattis du har gissat korrekt! Du lyckades på {attempts} försök.");
+                    return;
+                }
+                else if (guess < secretNumber)
+                {
+                    Console.WriteLine("Fel, Nummret jag tänker på är större. Försök igen.");
+                }
+                else
+                {
+                    Console.WriteLine("Fel, Numret jag tänker på är mindre. Försök igen.");
+                }
+        }
+        
+        Console.WriteLine($"\nDu har använt alla dina gissningar. Numret jag tänkte på var {secretNumber}. Bättre lycka nästa gång!");
+    }
+
+    static bool guessCheck(int guess, int secretNumber) // metod för att kontrollera om gissningen är korrekt
+    {
+        return guess == secretNumber;
+    }
+}
