b120ab18226e45e9a2f58eed257db273ca9a6f9d
alexanderek <Alexander.ek@live.se>
2024-09-06 16:15:14 +0200

    skapat upp en metod för att hantera huvudmenyn. Gör min kod lite renare men primärt för att jag vill öva på metoder. Även lagt till en bättre felhantering om användare anger text istället för siffror vid menyval eller inne i gissningsspelet.

--- NumbersGame/Program.cs
+++ NumbersGame/Program.cs
@@ -8,26 +8,12 @@ class Program
         
         while (isRunning) // loop to keep the meny running
         {
-            // welcome message and difficulty level selection
-            Console.WriteLine("\nVälkommen till mitt gissningsspel. Klarar du av att gissa rätt nummer?");
-            Console.WriteLine("\nBörja med att välja svårighetsgrad:");
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine("1. Lätt 1-10 5 försök");
-            Console.ResetColor(); 
-            Console.ForegroundColor = ConsoleColor.DarkYellow;
-            Console.WriteLine("2. Medel 1-20 4 försök");
-            Console.ResetColor();
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine("3. Svår 1-50 3 försök");
-            Console.ResetColor();
-            Console.ForegroundColor = ConsoleColor.White;
-            Console.WriteLine("4. Avsluta");
-            Console.ResetColor();
+            DisplayMenu(); // call the DisplayMenu method
 
             string input = Console.ReadLine(); // user input for difficulty level
 
-            int maxNumbers = 0; //  give my maxnumbers a value to avoid error
-            int maxAttempts = 0; // give my maxattempts a value to avoid error
+            int maxNumbers = 0; // Declare the variable to store the max guess, will be assigned a value in the switch case depending on difficulty level
+            int maxAttempts = 0; // Declare the variable to store the allowed number of attempts, will be assigned a value in the switch case depending on difficulty level
             
             switch (input)
             {
@@ -51,9 +37,28 @@ class Program
                     isRunning = false; // set isRunning to false to exit the loop
                     break;
                 default:
-                    Console.WriteLine("Ogiltligt val, försök igen."); // invalid input message instead of a crash
+                    Console.WriteLine("Ogiltligt val måste vara en siffra, försök igen."); // invalid input message instead of a crash
                     break;
             }
+        }
+
+        static void DisplayMenu() // method to display the menu
+        {
+            // welcome message and difficulty level selection
+            Console.WriteLine("\nVälkommen till mitt gissningsspel. Klarar du av att gissa rätt nummer?");
+            Console.WriteLine("\nBörja med att välja svårighetsgrad:");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("1. Lätt 1-10 5 försök");
+            Console.ResetColor(); 
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine("2. Medel 1-20 4 försök");
+            Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("3. Svår 1-50 3 försök");
+            Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("4. Avsluta");
+            Console.ResetColor();
         }
 
         static void PlayGame(int maxNumbers, int maxAttempts) // The game logic is created in my method PlayGame
@@ -66,8 +71,13 @@ class Program
 
             while (attempts < maxAttempts) // loop for the amount of maxAttempts
             {
-                int guess = int.Parse(Console.ReadLine()); // user input for guess
+                if (!int.TryParse(Console.ReadLine(), out int guess)) // check if the input is a number
+                {
+                    Console.WriteLine("Ogiltlig inmatning måste vara ett nummer, försök igen."); // invalid input message must be a number
+                    continue; // continue to the next iteration of the loop
+                }
                 attempts++; // increment attempts after each guess
+                
                 if (guessCheck(guess, secretNumber)) // call the guessCheck method to check if the guess is correct
                 {
                     Console.WriteLine($"Grattis du har gissat korrekt! Du lyckades på {attempts} försök.");
